e8bb3486b3661b51a430e903e550425c821fce92
Yu-Hsuan Ho <stanley30543@gmail.com>
2020-08-16 02:33:59 +0800

    Set up some initial files

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/item/:id', express.static(`${__dirname}/../public`));
+
+const port = 3214;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
