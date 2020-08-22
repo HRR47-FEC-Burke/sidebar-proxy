ca61c92939ae8b0be989cb698d1f7b1e5dae1011
Yu-Hsuan Ho <stanley30543@gmail.com>
2020-08-22 12:09:12 +0800

    Change to aws url

--- server/index.js
+++ server/index.js
@@ -3,33 +3,28 @@ const bodyParser = require('body-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const URL = process.env.URL || 'http://localhost';
 const PORT = process.env.PORT || '3214';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/item/:id', express.static(`${__dirname}/../public`));
 
-const mpScriptUrl = process.env.MP_URL || 'http://localhost:3001/bundle.js';
-const mpTarget = process.env.MP_TARGET || 'http://localhost:3001';
+const mpTarget = 'http://3.23.171.227';
 const mpProxy = { target: mpTarget, changeOrigin: true }
 app.use('*/mp/api', createProxyMiddleware(mpProxy));
 
-const srScriptUrl = process.env.SR_URL || 'http://localhost:2625/dist/bundle.js';
-const srTarget = process.env.SR_TARGET || 'http://localhost:2625';
+const srTarget = 'http://50.18.135.245';
 const srProxy = { target: srTarget, changeOrigin: true }
 app.use('*/sr/api', createProxyMiddleware(srProxy));
 
-const sbScriptUrl = process.env.SB_URL || 'http://localhost:3210/bundle.js';
-const sbTarget = process.env.SB_TARGET || 'http://localhost:3210';
+const sbTarget = 'http://15.164.155.96';
 const sbProxy = { target: sbTarget, changeOrigin: true }
 app.use('*/sb/api', createProxyMiddleware(sbProxy));
 
-const slnScriptUrl = process.env.SLN_URL || 'http://localhost:3005/similar-listings-news.bundle.js';
-const slnTarget = process.env.SLN_TARGET || 'http://localhost:3005';
+const slnTarget = 'http://50.18.3.201';
 const slnProxy = { target: slnTarget, changeOrigin: true }
 app.use('*/sln/api', createProxyMiddleware(slnProxy));
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${URL}:${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
